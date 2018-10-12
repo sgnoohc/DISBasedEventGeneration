b3350185c16df4a200e72326ca6d89204147fbb8
Philip Chang <sgnoohc@gmail.com>
2018-10-12 14:59:55 -0700

    First time implementing the condor executable output

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ def main():
 
     script_string = reroute_pileup_input_to_cached_files(script_string)
 
-    print script_string
+    get_condor_executable_script_string(script_string)
 
 #############################################################
 # Input : chained script string
@@ -321,6 +321,206 @@ def is_driver_formatted(driver):
     print "The cmsDriver command string that was used to search was :"
     print driver
     sys.exit(258)
+
+#############################################################
+# Input : local command script
+# Output : condor-ready executable
+def get_condor_executable_script_string(script_string):
+
+    # Print the header portion of the executable
+    print condor_executable_header
+
+    # The final output file to be copied via gfal-copy
+    final_output_file = ""
+
+    # Also save the .xml output paths so we can print out status
+    xml_outputs = []
+
+    # Loop over the lines and print out the scripts from the local files with few additional lines depending on the --datatier
+    for line in script_string.split("\n"):
+
+        # The run number and random seeds needs to be customized for each condor jobs.
+        # The special strings defined in customise_commands_<type> global string contains the necessary modifications.
+        # So as we print out the lines we add a lines depending on the conditions
+
+        # If LHE and not GEN-SIM then print customise_commands_LHE
+        if "--datatier" in line and "LHE" in line and "GEN-SIM" not in line:
+            print customise_commands_LHE
+            print line
+        # If not LHE and GEN-SIM then print customise_commands_GS
+        elif "--datatier" in line and "LHE" not in line and "GEN-SIM" in line:
+            # double check whether it's GEN-SIM and not GEN-SIM-RAW (n.b. there are GEN-SIM and also GEN-SIM-RAW while we need customise commands for GEN-SIM only)
+            ls = line.split()[1].split(",")
+            if "GEN-SIM" in ls:
+                print customise_commands_GS
+                print line
+            else:
+                print line
+        # If LHE and GEN-SIM then print customise_commands_LHEGS
+        elif "--datatier" in line and "LHE" in line and "GEN-SIM" in line:
+            print customise_commands_LHEGS
+            print line
+        else:
+            print line
+
+        # As we go through the lines we also pick up the "FINALOUTPUT" root file to be gfal-copied
+        if "--fileout" in line:
+            final_output_file = line.split()[1][5:]
+
+
+        # As we go through the lines we pick up the .xml paths
+        if "cmsRun " in line:
+            xml_outputs.append(line.split()[3])
+
+    # Set the final output file as FINALOUTPUT environment variable which will be used in the condor_executable_trailer to gfal-copy
+    print "FINALOUTPUT=%s \n" % (final_output_file)
+
+    for xml in xml_outputs:
+        print """
+# Printing out some information about the result of each step
+XML=%s
+echo "getting stats from "${XML}
+grep "TotalEvents" ${XML}
+grep "Timing-tstoragefile-write-totalMegabytes"  ${XML}
+grep "PeakValueRss" ${XML}
+grep "AvgEventTime" ${XML}
+grep "AvgEventCPU"  ${XML}
+grep "TotalJobCPU"  ${XML}
+grep "EventThroughput" ${XML}
+""" % xml
+
+    # Add the gfal-copy portion
+    print condor_executable_trailer
+
+#### Global "const" strings hard-coded for the mods needed for condor executable 
+
+customise_commands_GS    = "    --customise_commands \"process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(50) \\n process.source.firstRun = cms.untracked.uint32(${JOBNUM})\" \\"
+customise_commands_LHE   = "    --customise_commands \"process.RandomNumberGeneratorService.externalLHEProducer.initialSeed = ${RANDOMSEED}\" \\"
+customise_commands_LHEGS = "    --customise_commands \"process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(50) \\n process.source.firstRun = cms.untracked.uint32(${JOBNUM}) \\n process.RandomNumberGeneratorService.externalLHEProducer.initialSeed = ${RANDOMSEED}\" \\"
+
+condor_executable_header = """
+#!/bin/bash
+
+PACKAGE=package.tar.gz
+
+# Echo exact command
+echo "$(basename $0) $*"
+
+
+###################################################################################################
+# ProjectMetis/CondorTask specific (Setting up some common environment)
+###################################################################################################
+#echo "To check whether it's on condor universe Vanilla or Local. The following variables are used."
+#echo "If _CONDOR_SLOT is set, it's on Vanilla"
+#echo "If X509_USER_PROXY is set, it's either on Vanilla or Local."
+# if 
+if [ "x${_CONDOR_JOB_AD}" == "x" ]; then
+    :
+    INPUTFILENAMES=$1
+    shift;
+else
+    hostname
+    uname -a
+    date
+    whoami
+    pwd
+    echo "ls'ing hadoop"
+    ls -lh /hadoop/cms/store/user/phchang/
+    echo "_CONDOR_SLOT" ${_CONDOR_SLOT}
+    echo "X509_USER_PROXY" ${X509_USER_PROXY}
+    echo "_CONDOR_SCRATCH_DIR"             ${_CONDOR_SCRATCH_DIR}
+    echo "_CONDOR_SLOT"                    ${_CONDOR_SLOT}
+    echo "CONDOR_VM"                       ${CONDOR_VM}
+    echo "X509_USER_PROXY"                 ${X509_USER_PROXY}
+    echo "_CONDOR_JOB_AD"                  ${_CONDOR_JOB_AD}
+    echo "_CONDOR_MACHINE_AD"              ${_CONDOR_MACHINE_AD}
+    echo "_CONDOR_JOB_IWD"                 ${_CONDOR_JOB_IWD}
+    echo "_CONDOR_WRAPPER_ERROR_FILE"      ${_CONDOR_WRAPPER_ERROR_FILE}
+    echo "CONDOR_IDS"                      ${CONDOR_IDS}
+    echo "CONDOR_ID"                       ${CONDOR_ID}
+    OUTPUTDIR=$1
+    OUTPUTNAME=$2
+    INPUTFILENAMES=$3
+    IFILE=$4
+    CMSSWVERSION=$5
+    SCRAMARCH=$6
+    if [ "x${_CONDOR_SLOT}" == "x" ]; then
+        WORKDIR=/tmp/phchang_condor_local_${OUTPUTDIR//\//_}_${OUTPUTNAME}_${IFILE}
+        mkdir -p ${WORKDIR}
+        ls
+        cp package.tar.gz ${WORKDIR}
+        cd ${WORKDIR}
+        ls
+        echo "This is in Condor session with Universe=Local."
+        echo "WORKDIR=${WORKDIR}"
+        echo "pwd"
+        pwd
+    fi
+    echo "OUTPUTDIR     : $1"
+    echo "OUTPUTNAME    : $2"
+    echo "INPUTFILENAMES: $3"
+    echo "IFILE         : $4"
+    echo "CMSSWVERSION  : $5"
+    echo "SCRAMARCH     : $6"
+    shift 6
+    tar xvzf package.tar.gz
+    if [ $? -eq 0 ]; then
+        echo "Successfully untarred package."
+        :
+    else
+        echo "Failed to untar package."
+        exit
+    fi
+fi
+###################################################################################################
+
+#OUTPUTDIR=$1
+#OUTPUTNAME=$2
+#INPUTFILENAMES=$3
+#IFILE=$4
+#CMSSWVERSION=$5
+#SCRAMARCH=$6
+
+OUTDIR=${OUTPUTDIR}
+OUTPUT=${OUTPUTNAME}
+JOBNUM=${IFILE}
+RANDOMSEED=123${IFILE}
+TAG=""
+
+if [ -z ${OUTDIR} ]; then usage; fi
+if [ -z ${OUTPUT} ]; then usage; fi
+if [ -z ${JOBNUM} ]; then usage; fi
+if [ -z ${RANDOMSEED} ]; then usage; fi
+
+# Verbose
+date
+echo "================================================"
+echo "$(basename $0) $*"
+echo "$(basename $0) $*" >> $DIR/.$(basename $0).history
+echo "------------------------------------------------"
+echo "FRAGMENT          : ${FRAGMENT}"
+echo "OUTDIR            : ${OUTDIR}"
+echo "OUTPUT            : ${OUTPUT}"
+echo "JOBNUM            : ${JOBNUM}"
+echo "RANDOMSEED        : ${RANDOMSEED}"
+echo "CMSSWVERSION      : ${CMSSWVERSION}"
+echo "TAG               : ${TAG}"
+echo "================================================"
+
+
+"""
+
+condor_executable_trailer = """
+# For copying outputs when using some older CMSSW this stupid thing needs to be loaded
+export LD_PRELOAD=/usr/lib64/gfal2-plugins//libgfal_plugin_xrootd.so 
+
+gfal-copy -p -f -t 4200 --verbose file://`pwd`/${FINALOUTPUT} gsiftp://gftp.t2.ucsd.edu/${OUTPUTDIR}/${OUTPUTNAME}_${IFILE}.root --checksum ADLER32
+echo "Bye."
+
+date
+"""
+
+
 
 if __name__ == "__main__":
 
